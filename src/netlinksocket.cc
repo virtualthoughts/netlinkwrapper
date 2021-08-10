f4da3e7c23147bf947fe482bdda92eca939dcd12
Josh <virtualthoughts@gmail.com>
2021-08-10 12:40:06 -0400

    * modified to be "context-aware"

--- src/netlinksocket.cc
+++ src/netlinksocket.cc
@@ -1,10 +1,12 @@
 #include <node.h>
 #include "netlinkwrapper.h"
 
-void init_all(v8::Local<v8::Object> exports)
+
+extern "C" NODE_MODULE_EXPORT
+void NODE_MODULE_INITIALIZER(v8::Local<v8::Object> exports,
+                             v8::Local<v8::Value> module,
+                             v8::Local<v8::Context> context)
 {
     NL::init();
     NetLinkWrapper::init(exports);
 }
-
-NODE_MODULE(netlinksocket, init_all)
